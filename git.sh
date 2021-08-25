e7a9af00b456537e666e19bbe4acd343e71d93df
ilnarkamalov <kamalov.i.test@gmail.com>
2021-08-25 13:33:18 +0300

    init epte

---
+++ git.sh
@@ -0,0 +1,29 @@
+git init
+
+# First version, initial commit:
+git status
+git add mymodule.py
+git status
+git commit -m 'Initial commit'
+
+git status
+git log
+git show HEAD
+
+# Second change, second commit:
+git status
+git add mymodule.py
+git status
+git commit -m 'Adds another_function to mymodule.py'
+
+git status
+git log
+
+# Third change, third commit:
+git status
+git add mymodule.py
+git status
+git commit -m 'Adds comments'
+
+git status
+git log
---
+++ versions/third.py
@@ -0,0 +1,9 @@
+# Previously I had some complex logic in this
+# module, but I had to delete it due to reasons.
+# Very sad!
+
+def my_function(first, second):
+    return first + second
+
+def another_function(first):
+    return first + 1
